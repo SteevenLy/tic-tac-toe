81b7df2f82da1c6425fb104d41fe5c9b6fe92a82
STEEVEN LY <sl@smartapps.fr>
2015-11-17 14:17:38 +0100

    step 4

--- lib/app.jsx
+++ lib/app.jsx
@@ -4,15 +4,21 @@ var React = require('react');
 
 var styleBox = {
   height : '100px',
-  width : '100px'
+  width : '100px',
+  'font-size' : '30px'
 };
 
 var Box = React.createClass({
+
+  getInitialState: function() {
+    return {value: this.props.initialValue};
+  },
+
   'render': function onRender () {
     return (
-      <button style={styleBox}>{this.props.value}</button>
+      <button style={styleBox}>{this.state.value}</button>
     );
   }
 });
 
-React.render(<Box value='X'/>, document.body);
+React.render(<Box initialValue='X'/>, document.body);
